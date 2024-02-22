c5664a5a6bae8a62370b8fa9f025e67bd24ee6a8
Diego Sano <diegocsano@outlook.com>
2024-02-22 01:18:22 -0300

    feat: validate entry cli arguments

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import { argv } from 'bun';
+import { parseArgs } from 'util';
+
+function main() {
+  const { positionals } = parseArgs({
+    args: argv,
+    strict: true,
+    allowPositionals: true,
+  });
+
+  const args = positionals.slice(2);
+
+  if (args.length === 0) {
+    console.error('No entry URL provided');
+    process.exit(1);
+  }
+
+  if (args.length > 1) {
+    console.error(
+      'Incorrect number of arguments, you should pass only the entry URL'
+    );
+    process.exit(1);
+  }
+
+  const [baseURL] = args;
+  console.info(`Starting to crawl URL: ${baseURL} ...`);
+}
+
+main();
